8c490ea25d5397465ce8c8d4d3aa69b0e34f47d9
tushal024 <tushalshekh@gmail.com>
2024-09-14 17:38:44 +0530

    14-09-2024

---
+++ SUNDAY/reverse.js
@@ -0,0 +1,17 @@
+// const array1 = ['one', 'two', 'three'];
+// console.log('array1:', array1);
+// // Expected output: "array1:" Array ["one", "two", "three"]
+
+// const reversed = array1.reverse();
+// console.log('reversed:', reversed);
+// // Expected output: "reversed:" Array ["three", "two", "one"]
+
+// // Careful: reverse is destructive -- it changes the original array.
+// console.log('array1:', array1);
+// // Expected output: "array1:" Array ["three", "two", "one"]
+
+
+let a=["D","A","H","T"]
+
+a.reverse();
+console.log(a);
